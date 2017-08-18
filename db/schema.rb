94cb16ecc5981d90187d92aa06d3552fe92d40da
markjconlon <markjconlon@gmail.com>
2017-08-18 15:06:57 -0400

    added products model and controller ran migration

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170818174731) do
+ActiveRecord::Schema.define(version: 20170818190357) do
 
   create_table "admins", force: :cascade do |t|
     t.string   "email",                  default: "", null: false
@@ -27,6 +27,15 @@ ActiveRecord::Schema.define(version: 20170818174731) do
     t.datetime "updated_at",                          null: false
     t.index ["email"], name: "index_admins_on_email", unique: true
     t.index ["reset_password_token"], name: "index_admins_on_reset_password_token", unique: true
+  end
+
+  create_table "products", force: :cascade do |t|
+    t.string   "name"
+    t.text     "description"
+    t.integer  "price_in_cents"
+    t.integer  "upc"
+    t.datetime "created_at",     null: false
+    t.datetime "updated_at",     null: false
   end
 
 end

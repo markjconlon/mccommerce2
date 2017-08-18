04ba3094e0a2289631f4bcf49db59a82011b9905
markjconlon <markjconlon@gmail.com>
2017-08-18 15:22:37 -0400

    active admin setup

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,8 @@ Rails.application.routes.draw do
   get 'products/index'
 
   get 'products/show'
-
-  devise_for :admins
+  root 'products#index'
+  devise_for :admins, ActiveAdmin::Devise.config
+  ActiveAdmin.routes(self)
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
